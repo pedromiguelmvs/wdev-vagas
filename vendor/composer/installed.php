b21927e0c2c0b68c87eff8b418f32cbc1c36aee2
pedromiguelmvs <pedromiguelmvs@gmail.com>
2020-12-07 02:24:29 -0300

    autoload das classes incrementado

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1876ba817228817b12096ae363f964ec2c11c388',
+    'name' => 'wdev/vagas',
+  ),
+  'versions' => 
+  array (
+    'wdev/vagas' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1876ba817228817b12096ae363f964ec2c11c388',
+    ),
+  ),
+);
