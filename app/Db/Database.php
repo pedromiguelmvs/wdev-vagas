d25181105352665088aff44b5468067202fbf028
pedromiguelmvs <pedromiguelmvs@gmail.com>
2020-12-08 23:30:43 -0300

    Criação do arquivo de conexão com a DB

---
+++ app/Db/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Db;
+
+class Database {
+
+    /**
+     * Host de conexão com o banco de dados
+     * @var string
+     */
+    const HOST = 'localhost';
+
+    /**
+     * Nome da DB
+     * @var string
+     */
+
+    const NAME = 'wdev_vagas';
+
+    /**
+     * Usuário da DB
+     * @var string
+     */
+    const USER = 'root';
+
+    /**
+     * Senha de acesso ao banco de dados
+     * @var string
+     */
+    const PASS = '';
+
+}
